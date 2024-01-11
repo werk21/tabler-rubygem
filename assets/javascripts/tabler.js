031efcef4cc14324803eacb13f13f43d9f356bb0
Jan Kellermann <jan.kellermann@werk21.de>
2024-01-11 20:41:15 +0100

    Update jQuery to 3.5.1

--- assets/javascripts/tabler.js
+++ assets/javascripts/tabler.js
@@ -1,5 +1,5 @@
 // = require tabler/tabler
-// = require tabler/vendors/jquery-3.2.1.min
+// = require tabler/vendors/jquery-3.5.1.min
 // = require tabler/vendors/bootstrap.bundle.min
 // = require tabler/vendors/circle-progress.min
 // = require tabler/vendors/jquery.sparkline.min
--- tasks/updater/js.rb
+++ tasks/updater/js.rb
@@ -23,7 +23,7 @@ class Updater
       log_status 'Updating js main files'
       File.write "#{save_to}/../tabler.js", <<~eos
         // = require tabler/tabler
-        // = require tabler/vendors/jquery-3.2.1.min
+        // = require tabler/vendors/jquery-3.5.1.min
         // = require tabler/vendors/bootstrap.bundle.min
         // = require tabler/vendors/circle-progress.min
         // = require tabler/vendors/jquery.sparkline.min
